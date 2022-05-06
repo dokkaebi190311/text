690defcfb827b121953b8a0e807c91ce04487068
dokkaebi190311 <dokkaebi190311@gmail.com>
2022-05-06 13:35:20 +0900

    31520

--- hello.py
+++ hello.py
@@ -1,5 +1,14 @@
+#과제 : 1부터 10까지 반복하는 과정에서 3의 배수일 경우 ,year를 출력 하시오
+#나머지 모든 경우는 숫자 그대로 출력
+
+i=0
+
+print('과제')
+
+for i in range(1,10+1):
+    if i%3==0:
+        print("year")
+    print(i)
 
 
-
-print('hello')
 
